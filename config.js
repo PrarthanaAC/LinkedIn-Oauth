a01f7d43096118a5d3fe7e80e3076f17e3382b7d
Prarthana <prarthanac122@gmail.com>
2021-10-11 13:28:09 +0530

    init commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '78uhdunbvay5jq', // your App ID
+      'clientSecret': 'TNrMcev7PrXudR8A', // your App Secret
+
+      'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    }
+  }
